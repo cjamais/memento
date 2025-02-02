ddcdc76b452f4ae2bb33476c19708c977d09fb29
gabrielalb5 <gabrielalbino28@gmail.com>
2025-02-02 20:55:05 -0300

    Super atualização: - O código agora funciona no visual studio code - Arquivo makefile - Exibindo textos de rodada e erro - Fechando o jogo pelo X da aba

---
+++ Memento_V0.5/lib/jogo.h
@@ -0,0 +1,10 @@
+#include "addons.h"
+#define VERMELHO 1
+#define AMARELO 2
+#define VERDE 3
+#define AZUL 4
+
+void inicializar_posicoes(int posicoes[][2]);
+int identificar_cor_teclado(int tecla);
+int identificar_cor_mouse(int x, int y);
+void piscar_entrada(int entrada, ALLEGRO_BITMAP* prismaClaro, ALLEGRO_BITMAP* prismaPadrao);
---
+++ Memento_V0.5/src/main.c
@@ -0,0 +1,257 @@
+﻿#include "../lib/addons.h"
+#include "../lib/jogo.h"
+#include "../lib/queue.h"
+#define MAX_RODADAS 3
+
+int main() {
+
+    //PREPARAÇÃO DO AMBIENTE
+    if (!al_init()) {
+        printf("Erro ao inicializar o Allegro.\n");
+        return -1;
+    }
+
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+    ALLEGRO_FONT* font = al_create_builtin_font();
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
+    ALLEGRO_DISPLAY* tela = al_create_display(800, 800);
+    ALLEGRO_COLOR cores[4];
+
+    int posicoes[4][2];
+    inicializar_posicoes(posicoes);
+    al_register_event_source(event_queue, al_get_display_event_source(tela));
+
+    if (!event_queue || !font || !timer || !tela) {
+        printf("Erro ao criar recursos do Allegro.\n");
+
+        if (font) al_destroy_font(font);
+        if (timer) al_destroy_timer(timer);
+        if (event_queue) al_destroy_event_queue(event_queue);
+        if (tela) al_destroy_display(tela);
+
+        return -1;
+    }
+
+    iniciar_addons(tela, event_queue, font, timer);
+
+    ALLEGRO_BITMAP* prismaPadrao = al_load_bitmap("assets/img/prisma.png");
+    ALLEGRO_BITMAP* prismaClaro = al_load_bitmap("assets/img/prisma_claro.png");
+    ALLEGRO_FONT* font2 = al_load_font("assets/fonts/Poppins-Medium.ttf", 24, 0);
+    if (!font2) {
+        printf("Erro ao carregar a fonte.\n");
+        return -1;
+    }
+
+    if (!prismaPadrao) {
+        printf("Erro ao carregar a imagem prisma.png.\n");
+        al_destroy_font(font);
+        al_destroy_timer(timer);
+        al_destroy_event_queue(event_queue);
+        al_destroy_display(tela);
+        return -1;
+    }
+
+    bool running = true;
+
+    //LOOP PRINCIPAL DO JOGO
+    while (running) {
+        ALLEGRO_EVENT event;
+        al_wait_for_event(event_queue, &event);
+
+        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            running = false;
+            break;
+        }
+
+        //PREPRARAÇÃO DA FILA
+        T_Queue fila_original, sequencia, fila_copia;
+        T_Info saiu, esperado, entrada_usuario;
+        int errou = 0;
+
+        fila_original = init(MAX_RODADAS);
+        sequencia = init(MAX_RODADAS);
+        fila_copia = init(MAX_RODADAS);
+
+        srand(time(NULL));
+
+        for (int i = 0; i < MAX_RODADAS; i++) {
+            int random = (rand() % 4) + 1;
+            printf("%d %s\n", random, enqueue(fila_original, random) ? "Entrou" : "Não entrou");
+        }
+
+		desenhar_prisma(prismaPadrao);
+
+        int rodada = 1;
+        while (rodada <= MAX_RODADAS && !errou) {
+            
+            char texto_rodada[50];
+            sprintf(texto_rodada, "RODADA %d", rodada);
+
+            int y_texto = 50;
+            int x_texto = (800 - al_get_text_width(font2, texto_rodada)) / 2;
+            al_draw_text(font2, al_map_rgb(50, 50, 50), x_texto, y_texto, 0, texto_rodada);
+
+            printf("\nRodada %d\nCores que piscaram:", rodada);
+            for (int i = 0; i < rodada; i++) {
+                dequeue(fila_original, &saiu);
+                enqueue(sequencia, saiu);
+                enqueue(fila_copia, saiu);
+                printf(" %d", saiu);
+
+                // EXIBINDO AS CORES DA RODADA
+                switch (saiu) {
+                    case 1:
+                        // VERMELHO
+                        al_draw_bitmap_region(prismaClaro, 0, 0, 200, 200, 300, 175, 0);
+                        break;
+                    case 2:
+                        // AMARELO
+                        al_draw_bitmap_region(prismaClaro, 200, 0, 200, 200, 300, 425, 0);
+                        break;
+                    case 3:
+                        // VERDE
+                        al_draw_bitmap_region(prismaClaro, 400, 0, 200, 200, 175, 300, 0);
+                        break;
+                    case 4:
+                        // AZUL
+                        al_draw_bitmap_region(prismaClaro, 600, 0, 200, 200, 425, 300, 0);
+                        break;
+                    default:
+                        break;
+                }
+                
+                al_flip_display();
+
+                double start_time = al_get_time();
+                while (al_get_time() - start_time < 0.7) {
+                    while (al_get_next_event(event_queue, &event)) {
+                        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                            running = false;
+                            errou = 1;
+                            break;
+                        }
+                    }
+                    if (!running) break;
+                }
+
+                desenhar_prisma(prismaPadrao);
+                al_draw_text(font2, al_map_rgb(50, 50, 50), x_texto, y_texto, 0, texto_rodada);
+                al_flip_display();
+
+                start_time = al_get_time();
+                while (al_get_time() - start_time < 0.3) {
+                    while (al_get_next_event(event_queue, &event)) {
+                        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                            running = false;
+                            errou = 1;
+                            break;
+                        }
+                    }
+                    if (!running) break;
+                }
+
+                if (!running) break;
+            }
+
+            while (!is_empty(fila_original)) {
+                dequeue(fila_original, &saiu);
+                enqueue(fila_copia, saiu);
+            }
+
+            al_flush_event_queue(event_queue);
+
+			//WHILE DA ENTRADA DO USUÁRIO
+            while (!is_empty(sequencia)) {
+                dequeue(sequencia, &esperado);
+
+                bool entrada_valida = false;
+                int entrada = 0;
+
+                al_draw_text(font2, al_map_rgb(50, 50, 50), x_texto, y_texto, 0, texto_rodada);
+                char texto_sua_vez[50];
+                sprintf(texto_sua_vez, "Sua vez");
+                int x_texto_sua_vez = (800 - al_get_text_width(font2, texto_sua_vez)) / 2;
+                al_draw_text(font2, al_map_rgb(100, 100, 100), x_texto_sua_vez, y_texto+50, 0, texto_sua_vez);
+                al_flip_display();
+
+                while (!entrada_valida) {
+                    al_wait_for_event(event_queue, &event);
+
+                    if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                        running = false;
+                        errou = 1;
+                        break;
+                    }
+                    if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+                        entrada = identificar_cor_teclado(event.keyboard.keycode);
+                        entrada_valida = (entrada > 0 && entrada <= 4);
+                    }
+                    else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+                        entrada = identificar_cor_mouse(event.mouse.x, event.mouse.y);
+                        entrada_valida = (entrada > 0 && entrada <= 4);
+						
+                    }
+                }
+
+                piscar_entrada(entrada, prismaClaro, prismaPadrao);
+
+                if (entrada == esperado) {
+                    printf("OK\n");
+                } else {
+                    if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+                        running = false;
+                        errou = 1;
+                        break;
+                    }
+                    char texto_erro[50];
+                    char texto_enter[50];
+                    sprintf(texto_erro, "Errou");
+                    int x_texto_erro = (800 - al_get_text_width(font2, texto_erro)) / 2;
+                    al_draw_text(font2, al_map_rgb(255, 87, 51), x_texto_erro, y_texto, 0, texto_erro);
+                    sprintf(texto_enter, "Pressione ENTER para jogar novamente");
+                    int x_texto_enter = (800 - al_get_text_width(font2, texto_enter)) / 2;
+                    al_draw_text(font2, al_map_rgb(100, 100, 100), x_texto_enter, y_texto+50, 0, texto_enter);
+                    
+                    al_flip_display();
+                    
+                    printf("ERROU! A sequência era %d.\n", esperado);
+                    printf("Pressione ENTER para jogar novamente.\n");
+                    
+                    bool enter_pressionado = false;
+                    while (!enter_pressionado) {
+                        ALLEGRO_EVENT event;
+                        al_wait_for_event(event_queue, &event);
+
+                        if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+                            if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
+                                enter_pressionado = true;
+                            }
+                        }
+                    }
+                    errou = 1;
+                    break;
+                }
+            }
+
+            while (!is_empty(fila_copia)) {
+                dequeue(fila_copia, &saiu);
+                enqueue(fila_original, saiu);
+            }
+            rodada++;
+            al_rest(0.5);
+        }
+
+        destroy(fila_copia);
+        destroy(sequencia);
+        destroy(fila_original);
+    }
+
+    al_destroy_bitmap(prismaPadrao);
+    al_destroy_font(font);
+    al_destroy_font(font2);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(event_queue);
+    al_destroy_display(tela);
+
+    return 0;
+}
